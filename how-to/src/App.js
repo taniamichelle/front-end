d6fc4e8b0c3d28ed9a0b3225e82b848ce5f2f0c1
Elvis Knapman <elvis@elvisknapman.com>
2019-08-26 15:21:26 -0400

    Added basic routes to test

--- how-to/src/App.js
+++ how-to/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./App.css";
-import Tutorials from './components/Tutorials';
+import { Route, Link } from "react-router-dom";
+
+import Tutorials from "./components/Tutorials";
 
 // components
+import Home from "./components/Home/Home";
 import MyAccount from "./components/MyAccount/MyAccount";
 
 function App() {
   return (
     <div className="App">
-      <h1>How-To App</h1>
-      <MyAccount />
+      <Route exact path="/" component={Home} />
+      <Route path="/myaccount" component={MyAccount} />
     </div>
   );
 }
