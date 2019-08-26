a88dd6a23c1b166054265d42059fb94371d8eeb0
Elvis Knapman <elvis@elvisknapman.com>
2019-08-26 14:57:14 -0400

    set up basic form, using state to control inputs

--- how-to/src/components/MyAccount/ChangePassword.js
+++ how-to/src/components/MyAccount/ChangePassword.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ChangePassword = props => {
+  const [newPassword, setNewPassword] = useState({
+    password: "",
+    confirmPassword: ""
+  });
+
+  const handleChange = e => {
+    setNewPassword({
+      ...newPassword,
+      [e.target.name]: e.target.value
+    });
+  };
+
   return (
     <div>
       <h4>Change Password</h4>
@@ -8,13 +20,23 @@ const ChangePassword = props => {
         <div>
           <label>New Password</label>
           <div>
-            <input type="password" />
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              value={newPassword.password}
+            />
           </div>
         </div>
         <div>
           <label>Confirm New Password</label>
           <div>
-            <input type="password" />
+            <input
+              type="password"
+              name="confirmPassword"
+              onChange={handleChange}
+              value={newPassword.confirmPassword}
+            />
           </div>
         </div>
       </form>
