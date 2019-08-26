994df6729b15f2415f7c4bf605a636f2659dac46
Elvis Knapman <elvis@elvisknapman.com>
2019-08-26 19:35:25 -0400

    Connected to redux store

--- how-to/src/components/MyAccount/MyAccount.js
+++ how-to/src/components/MyAccount/MyAccount.js
@@ -2,35 +2,62 @@ import React, { useState } from "react";
 import { Route, Link } from "react-router-dom";
 import styled, { createGlobalStyle } from "styled-components";
 
+import { connect } from "react-redux";
+
 // components
 import AccountInfo from "./AccountInfo";
 import ChangePassword from "./ChangePassword";
 import MyTutorials from "../MyTutorials/MyTutorials";
 
+// actions
+import { changePassword } from "../../actions";
+
 // styles
 import GlobalStyle from "../../styled-components/GlobalStyle";
 import ContentContainer from "../../styled-components/ContentContainer";
 
-const MyAccount = props => {
-  console.log(props);
+const MyAccount = ({
+  match,
+  isChangingPassword,
+  errorMessage,
+  password,
+  changePassword
+}) => {
   const [person, setPerson] = useState({
     name: "Elvis Knapman",
     email: "elvisknapman@gmail.com"
   });
   return (
     <div>
+      <p>{password && password}</p>
       <h1>My Account</h1>
       <div>
-        <Link to={`${props.match.path}/changepassword`}>Change Password </Link>
+        <Link to={`${match.path}/changepassword`}>Change Password </Link>
       </div>
       <div>
-        <Link to={`${props.match.path}/mytutorials`}>My Tutorials</Link>
+        <Link to={`${match.path}/mytutorials`}>My Tutorials</Link>
       </div>
       <AccountInfo person={person} />
-      <Route path="/myaccount/changepassword" component={ChangePassword} />
+      <Route
+        path="/myaccount/changepassword"
+        render={props => (
+          <ChangePassword {...props} changePassword={changePassword} />
+        )}
+      />
       <Route path="/myaccount/mytutorials" component={MyTutorials} />
     </div>
   );
 };
 
-export default MyAccount;
+const mapStateToProps = state => {
+  return {
+    isChangingPassword: state.changePasswordReducer.isChangingPassword,
+    errorMessage: state.changePasswordReducer.errorMessage,
+    password: state.changePasswordReducer.password
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { changePassword }
+)(MyAccount);
