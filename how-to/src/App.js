e335dc37833deeef5ab46afd2aead3d61676f190
Elvis Knapman <elvis@elvisknapman.com>
2019-08-26 15:59:34 -0400

    Organzied

--- how-to/src/App.js
+++ how-to/src/App.js
@@ -2,27 +2,14 @@ import React from "react";
 import "./App.css";
 
 import styled, { createGlobalStyle } from "styled-components";
-import Tutorials from "./components/Tutorials";
 
-import { Route, Link } from "react-router-dom";
-
-import Tutorials from "./components/Tutorials";
+import { Route, Link, Switch } from "react-router-dom";
 
 // components
 import Home from "./components/Home/Home";
 import MyAccount from "./components/MyAccount/MyAccount";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: #feffe5;
-  }
-`;
-
-const Content = styled.div`
-  border: 15px solid #000e33;
-  border-radius: 30px;
-  padding: 50px;
-`;
+import Tutorials from "./components/Tutorials";
+import MyTutorials from "./components/MyTutorials/MyTutorials";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -39,11 +26,12 @@ const Content = styled.div`
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
-      <h1>How-To App</h1>
-      <Content>Content Div</Content>
-      <Route exact path="/" component={Home} />
-      <Route path="/myaccount" component={MyAccount} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/myaccount" component={MyAccount} />
+        <GlobalStyle />
+        <Content>Content Div</Content>
+      </Switch>
     </div>
   );
 }
