bcd0ef3393efaff596a7d1fc5b2276f74e851218
Jonathan Ho <johnnyho82@gmail.com>
2019-08-26 16:53:52 -0400

    Add functionality to SignUp page (still needs styles)

--- how-to/src/components/SignUp/SignUp.js
+++ how-to/src/components/SignUp/SignUp.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const FormWrapper = styled.div`
   border: 2px solid gray;
@@ -11,7 +12,7 @@ const FormWrapper = styled.div`
   align-items: center;
 `;
 
-const SignUp = () => {
+const SignUp = ({ touched, errors, values, status }) => {
   return (
     <div>
       <h1>It's time to start teaching each other!</h1>
@@ -20,20 +21,35 @@ const SignUp = () => {
             <h2>Sign Up</h2>
             <label>First Name</label>
             <Field name='firstname' type='text' placeholder='Type first name' />
+            {touched.firstname && errors.firstname && (
+              <p>{errors.firstname}</p>
+            )}
             <br />
             <label>Last Name</label>
             <Field name='lastname' type='text' placeholder='Type last name' />
+            {touched.lastname && errors.lastname && (
+              <p>{errors.lastname}</p>
+            )}
             <br />
             <label>Email Address</label>
             <Field name='email' type='text' placeholder='Type email address' />
+            {touched.email && errors.email && (
+              <p>{errors.email}</p>
+            )}
             <br />
             <label>Username</label>
             <Field name='username' type='text' placeholder='Type username' />
+            {touched.username && errors.username && (
+              <p>{errors.username}</p>
+            )}
             <br />
             <label>Password</label>
-            <Field name='password' type='text' placeholder='Type password' />
+            <Field name='password' type='password' placeholder='Type password' />
+            {touched.password && errors.password && (
+              <p>{errors.password}</p>
+            )}
             <br />
-            <button>Let's Go!</button>
+            <button type='submit'>Let's Go!</button>
           </FormWrapper>
         </Form>
     </div>
@@ -41,10 +57,31 @@ const SignUp = () => {
 }
 
 const FormikSignUp = withFormik({
-  mapPropsToValues({}) {
+  mapPropsToValues({ firstname, lastname, email, username, password }) {
     return {
-
+      firstname: firstname || '',
+      lastname: lastname || '',
+      email: email || '',
+      username: username || '',
+      password: password || '',
     }
+  },
+
+  validationSchema: Yup.object().shape({
+    firstname: Yup.string().required('First name required'),
+    lastname: Yup.string().required('Last name required'),
+    email: Yup.string().email().required('Not a valid email address'),
+    username: Yup.string().required('Please choose a username'),
+    password: Yup.string().required('Please enter a password'),
+  }),
+
+  handleSubmit(values, { setStatus }) {
+    axios
+      .post('https://reqres.in/api/users', values)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err));
   }
 })(SignUp);
 
