cf58c5e91d1347ebf89a7a74cf9e5902388f825c
tania <tanislavens@yahoo.com>
2019-08-26 14:36:54 -0400

    wip: installed react router dom, added components folder, built simple Tutorials form

--- how-to/src/App.js
+++ how-to/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Tutorials from './components/Tutorials';
 
 function App() {
   return (
---
+++ how-to/src/components/Tutorials.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Tutorials = props => {
+    const [tutorial, setTutorial] = useState({});
+
+    const handleChange = event => setTutorial({ ...tutorial, [event.target.name]: event.target.value });
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        axios.post('https://jsonplaceholder.typicode.com/posts')
+            .then(res => console.log(res))
+            .catch(err => console.log(err.response));
+    };
+
+    return (
+        <Router>
+            <div className='tutorials-container'>
+                <header>
+                    <h3>How-To</h3>
+                    <Link to='/search'>Search</Link>
+                    <Link to='/profile'>Profile</Link>
+                    <Link to='/sign out'>Sign Out</Link>
+                </header>
+                <div className='tutorials-content'>
+                    <h1>How to Title</h1>
+                    <button>Save!</button>
+                    <button>Helpful!</button>
+                    <div className='tutorials-form'>
+                        <h3>Description</h3>
+                        <form onSubmit={handleSubmit}>
+                            <input
+                                type='text'
+                                title='description'
+                                name='description'
+                                placeholder='Enter Description'
+                                value={tutorial.description}
+                                onChange={handleChange}
+                            />
+                            <button type='submit'>Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </Router>
+    )
+}
+
+export default Tutorials;
