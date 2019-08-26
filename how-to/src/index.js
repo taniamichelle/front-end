05eefeb2e19271e3e9a4a87959237f18061a3292
Elvis Knapman <elvis@elvisknapman.com>
2019-08-26 19:36:07 -0400

    created redux store

--- how-to/src/index.js
+++ how-to/src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import AppElvis from "./AppElvis";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+// reducer
+import rootReducer from "./reducers";
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <Router>
-    <AppElvis />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <AppElvis />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
