39be3894f13b4ec6c360de38c9761c022699661a
Elvis Knapman <elvis@elvisknapman.com>
2019-08-26 14:57:42 -0400

    inserted MyAccount component

--- how-to/src/App.js
+++ how-to/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
 
+// components
+import MyAccount from "./components/MyAccount/MyAccount";
+
 function App() {
   return (
     <div className="App">
       <h1>How-To App</h1>
+      <MyAccount />
     </div>
   );
 }
