f561d17c54eadb8000c7be09df1ac90370b01043
Jonathan Ho <johnnyho82@gmail.com>
2019-08-27 13:59:30 -0400

    Update styles for Search page

--- how-to/src/components/Search/Search.js
+++ how-to/src/components/Search/Search.js
@@ -1,6 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Field, withFormik } from 'formik';
+import styled from 'styled-components';
+
+const SearchStyles = styled.div`
+  h2 {
+    font-size: 36px;
+  }
+  
+  h3 {
+    font-size: 30px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .searchbar {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+
+      input {
+        width: 95%;
+        border: 2px solid gray;
+        border-radius: 3px;
+        padding: 5px 10px;
+        font-size: 18px;
+      }
+
+      button {
+        width: 5%;
+      }
+    }
+  }
+
+  .requestBtn {
+    align-self: flex-end;
+    cursor: pointer;
+    background-color: #0036cc;
+    color: white;
+    border: none;
+    outline: none;
+    font-size: 14px;
+    font-weight: bold;
+    padding: 5px;
+    border-radius: 3px;
+  }
+`;
 
 const tempData = [
   {
@@ -23,7 +71,7 @@ const NewContent = () => {
       <h3>What's New?</h3>
       {tempData.map(tutorial => (
         <div>
-          <Link to='/tutorial'><p>{tutorial.description}</p></Link>
+          <Link to='/tutorial'><img alt='video' src={tutorial.img}></img><p>{tutorial.description}</p></Link>
         </div>
       ))}
     </div>
@@ -40,17 +88,19 @@ const Filter = () => {
 
 const Search = ({ values }) => {
   return (
-    <div>
+    <SearchStyles>
       <h2>Let's Learn!</h2>
       <Form>
-        <Field name='search' type='text' placeholder='Search' />
-        <button><span>🔍</span></button>
+        <div className='searchbar'>
+          <Field name='search' type='text' placeholder='Search' />
+          <button><span>🔍</span></button>
+        </div>
         <br />
-        <button>Request Tutorial</button>
+        <button className='requestBtn'>Request<br />Tutorial</button>
         <Filter />
         <NewContent />
       </Form>
-    </div>
+    </SearchStyles>
   )
 }
 
