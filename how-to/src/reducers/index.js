a48d4678e6caad4d6493b3ce2db2655763b1a6a1
Elvis Knapman <elvis@elvisknapman.com>
2019-08-27 17:48:49 -0400

    :sparkles: added loginReducer to combineReducers

--- how-to/src/reducers/index.js
+++ how-to/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 
 // reducers
-import changePasswordReducer from "./changePassword";
-import tutorialReducer from './tutorialReducer';
+import changePasswordReducer from "./changePasswordReducer";
+import tutorialReducer from "./tutorialReducer";
+import loginReducer from "./loginReducer";
 
 const rootReducer = combineReducers({
+  loginReducer,
   changePasswordReducer,
   tutorialReducer
 });
