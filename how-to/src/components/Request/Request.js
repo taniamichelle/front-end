2c04b26cd5c252d2c70610543fcb38f0724fd609
umbur <you@example.com>
2019-08-27 17:39:51 -0600

    post request works, added some styling

--- how-to/src/components/Request/Request.js
+++ how-to/src/components/Request/Request.js
@@ -16,14 +16,31 @@ const Button = styled.button`
   width: 92px;
 `;
 
+const SubmitButton = styled.button`
+    display: inline-block;
+    color: white;
+    background-color: blue;
+    font-size: 1em;
+    margin-left: 38em;
+    margin-top: -2.9em;
+    padding: 0.25em 1em;
+    border: 2px solid blue;
+    border-radius: 3px;
+    display: block;
+    width: 92px;
+`;
+
+
 class Request extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       modal: false,
-      textContent: ''
+      name: '',
+      job: ''
     };
 
+    // this.handleSubmit = this.handleSubmit.bind(this)
     this.toggle = this.toggle.bind(this);
   }
 
@@ -33,13 +50,20 @@ class Request extends React.Component {
     }));
   }
 
-  handleSubmit(textContent) {
+  handleSubmit = () => {
+      console.log('handleSubmit fired!')
     axios
-      .post('https://reqres.in/api/users', textContent)
+      .post('https://reqres.in/api/users', {name: this.state.name, job: this.state.job})
       .then(res => {
+        // this.setState({name: res.data.name, job: res.data.job})
+        this.setState({name: '', job: ''})
         console.log(res);
       })
       .catch(err => console.log(err));
+  }
+
+  handleChanges = (e) => {
+      this.setState( { [e.target.name] : e.target.value } )
   }
 
   render() {
@@ -47,19 +71,19 @@ class Request extends React.Component {
       <div>
         <Button onClick={this.toggle}>{this.props.buttonLabel} Request Tutorial </Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}><h1>Tutorial Request Form</h1></ModalHeader>
+          <h1>Tutorial Request Form</h1>
           <ModalBody>
               <h2>What do you need a tutorial of? Please be detailed.</h2>
         <FormGroup>
           <Label for="exampleText"></Label>
           
-          <Input type="textarea" name="text" id="exampleText" />
+          <Input type="textarea" onChange={this.handleChanges} value={this.state.name} rows="12" cols="100" name="name" id="exampleText" />
         </FormGroup>
            
           </ModalBody>
           <ModalFooter>
             <Button onClick={this.toggle}>Cancel</Button>{' '}
-            <Button onClick={this.handleSubmit}>Submit</Button>
+            <SubmitButton onClick={this.handleSubmit}>Submit</SubmitButton>
           </ModalFooter>
         </Modal>
       </div>
