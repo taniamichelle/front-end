9f9df50065ca97c4d51c4f15e3a3bfacf5ff30b1
Jonathan Ho <johnnyho82@gmail.com>
2019-08-27 18:59:29 -0400

    Basic search & filter functionality

--- how-to/src/components/Search/Filters.js
+++ how-to/src/components/Search/Filters.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Route, Link } from 'react-router-dom';
 
 const FilterStyles = styled.div`
   width: 100%;
 
-  .categories {
+  .searchfields {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -32,51 +33,106 @@ const FilterStyles = styled.div`
       border-radius: 3px;
     }
   }
+
+  ul {
+    padding: 0;
+
+    li {
+      width: 170px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      font-size: 20px;
+
+      img {
+        width: 50px;
+      }  
+    }
+  }
+
+  .categories {
+    display: flex;
+    align-items: center;
+  }
+
+  .subwrap {
+    margin-left: 30px;
+  }
+
+  .subs {
+    max-height: 300px;
+    width: 500px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+
+    p {
+      font-size: 18px;
+      margin: 9px 0;
+    }
+  }
 `;
 
 const categories = [
   {
     category: 'Appliances',
-    subcategories: []
+    subcategories: ['Sub1', 'Sub2', 'Sub3']
   },
   {
     category: 'Arts & Crafts',
-    subcategories: []
+    subcategories: ['Sub1', 'Sub2', 'Sub3']
   },
   {
     category: 'Automotive',
-    subcategories: []
+    subcategories: ['Sub1', 'Sub2', 'Sub3']
   },
   {
     category: 'Education',
-    subcategories: []
+    subcategories: ['Sub1', 'Sub2', 'Sub3']
   },
   {
     category: 'Electronics',
-    subcategories: []
+    subcategories: ['Sub1', 'Sub2', 'Sub3']
   },
   {
     category: 'Fishing',
-    subcategories: []
+    subcategories: ['Sub1', 'Sub2', 'Sub3']
   },
   {
     category: 'Home',
-    subcategories: []
+    subcategories: ['Beds', 'Cabinets & Countertops', 'Curtains/Blinds/Shutters', 'Desking & Railing', 'Drywall', 'Fencing', 'Flooring', 'Garage Doors', 'Gutters & Downspouts', 'Heating & Air', 'Hot Tubs, Spas, & Pools', 'Kitchen', 'Lighting', 'Windows & Doors']
   },
   {
     category: 'Hunting',
-    subcategories: []
+    subcategories: ['Sub1', 'Sub2', 'Sub3']
   },
   {
     category: 'Yard',
-    subcategories: []
+    subcategories: ['Sub1', 'Sub2', 'Sub3']
   },
 ]
+
+const Subcategory = props => {
+  return (
+    <div className='subwrap'>
+      <h3>{props.category}</h3>
+      <div className='subs'>
+      {props.subs.map(
+        sub => (
+        <p key={sub}>
+          <input type='checkbox' />
+          {sub}
+        </p>
+      ))}
+      </div>
+    </div>
+  )
+}
 
 export default function Filters() {
   return (
     <FilterStyles>
-      <div className='categories'>
+      <div className='searchfields'>
         <h3>Categories</h3>
         <div>
           <h4>Keyword Search</h4>
@@ -88,13 +144,25 @@ export default function Filters() {
         </div>
         <button>Apply</button>
       </div>
-      <ul>
-        {
-          categories.map(category => (
-            <li>{category.category}</li>
-          ))
-        }
-      </ul>
+      <div className='categories'>
+        <ul>
+          {categories.map(category => (
+            <li key={category.category}>{category.category}
+              <Link to={`/search/filter/${category.category}`}>
+                <img 
+                  alt='filter arrow' 
+                  src={require('../../images/filter-arrow.png')}
+                />
+              </Link>
+            </li>
+          ))}
+        </ul>
+        {categories.map(category => (
+          <Route key={category.category} path={`/search/filter/${category.category}`} 
+            render={props => <Subcategory {...props} category={category.category} subs={category.subcategories} />}
+        />
+        ))}
+      </div>
     </FilterStyles>
   )
 }
