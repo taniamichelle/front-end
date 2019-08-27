abe74bb282d9e88e87ffa117aeb2fa6cc91b7bfb
Jonathan Ho <johnnyho82@gmail.com>
2019-08-27 11:11:35 -0400

    Adjust container styles

--- how-to/src/styled-components/ContentContainer.js
+++ how-to/src/styled-components/ContentContainer.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const ContentContainer = styled.div`
   border: 15px solid #000e33;
   border-radius: 30px;
-  padding: 0px 50px 50px;
+  padding: 20px 50px 50px;
   width: 80%;
   margin: 20px auto;
 `;
