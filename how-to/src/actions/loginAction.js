c9e1371a682ae77f1d8c18de2fc42fb555e7a0f7
Elvis Knapman <elvis@elvisknapman.com>
2019-08-27 17:48:01 -0400

    :construction: changed request type
    changed from GET to POST

--- how-to/src/actions/loginAction.js
+++ how-to/src/actions/loginAction.js
@@ -5,11 +5,11 @@ export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
-export const login = id => dispatch => {
+export const login = user => dispatch => {
   dispatch({ type: LOGIN_START });
 
   axios
-    .get(`http://someurl.com/${id}`)
+    .post(`http://someurl.com/users/`, user)
     .then(res => {
       localStorage.getItem("token");
       dispatch({ type: LOGIN_SUCCESS, payload: res.data });
