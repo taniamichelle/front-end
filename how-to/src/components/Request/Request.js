76eff62f41c969030c36c58a1265d1ee20ae314d
umbur <you@example.com>
2019-08-27 14:24:04 -0600

    created request button with pop-up form

--- how-to/src/components/Request/Request.js
+++ how-to/src/components/Request/Request.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import { Modal, ModalHeader, ModalBody, ModalFooter, FormText, Label, Input, FormGroup } from 'reactstrap';
 import styled from 'styled-components';
 
 const Button = styled.button`
@@ -14,14 +16,55 @@ const Button = styled.button`
   width: 92px;
 `;
 
-function Request() {
-    return(
-        <>
-        Hello from request
-        <Button>Request Tutorial</Button>
-        </>
-    )
-    
+class Request extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+      textContent: ''
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  }
+
+  handleSubmit(textContent) {
+    axios
+      .post('https://reqres.in/api/users', textContent)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.toggle}>{this.props.buttonLabel} Request Tutorial </Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}><h1>Tutorial Request Form</h1></ModalHeader>
+          <ModalBody>
+              <h2>What do you need a tutorial of? Please be detailed.</h2>
+        <FormGroup>
+          <Label for="exampleText"></Label>
+          
+          <Input type="textarea" name="text" id="exampleText" />
+        </FormGroup>
+           
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={this.toggle}>Cancel</Button>{' '}
+            <Button onClick={this.handleSubmit}>Submit</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default Request;
