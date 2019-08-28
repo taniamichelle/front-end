000a12fb7c1d2fb505119a11191762b5f615f676
tania <tanislavens@yahoo.com>
2019-08-28 15:12:05 -0400

    wip: save before pull

--- how-to/src/App.js
+++ how-to/src/App.js
@@ -14,7 +14,6 @@ import Search from './components/Search/Search';
 import MyAccount from './components/MyAccount/MyAccount';
 import UploadForm from './components/Tutorials/UploadForm';
 import Tutorials from './components/Tutorials/Tutorials';
-import UploadForm from './components/Tutorials/UploadForm';
 
 export default function App() {
   return (
