164587ab25e9be1b1d6fe382eeb19036da5fcc67
Elvis Knapman <elvis@elvisknapman.com>
2019-08-28 14:20:42 -0400

    :recycle: Changed form from Formik
    Changed from from formik due to conflict with Redux

--- how-to/src/components/SignIn/SignIn.js
+++ how-to/src/components/SignIn/SignIn.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Form, Field, withFormik } from 'formik';
-import * as Yup from 'yup';
-import axios from 'axios';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
 
-import FormWrapper from '../../styled-components/FormWrapper';
+import FormWrapper from "../../styled-components/FormWrapper";
+
+// actions
+import { login } from "../../actions/loginAction";
 
 const SignInDiv = styled.div`
   display: flex;
@@ -13,14 +13,33 @@ const SignInDiv = styled.div`
 `;
 
 const WelcomeDiv = styled.div`
-  margin-right: 20px;  
+  margin-right: 20px;
 
   p {
     text-align: left;
   }
 `;
 
-const SignIn = ({ touched, errors, values, status }) => {
+const SignIn = props => {
+  const { login, history } = props;
+
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    login(user, history);
+  };
+
   return (
     <SignInDiv>
       <WelcomeDiv>
@@ -28,57 +47,36 @@ const SignIn = ({ touched, errors, values, status }) => {
         <p>We learned a lot while you were gone!</p>
         <p>We're excited to share it with you!</p>
       </WelcomeDiv>
-      <Form>
+      <form onSubmit={handleSubmit}>
         <FormWrapper>
           <h2>Sign In</h2>
-          <label>Email Address</label>
-            <Field name='email' type='text' placeholder='Type email address' />
-            {touched.email && errors.email && (
-              <p>{errors.email}</p>
-            )}
-            <br />
-            <label>Username</label>
-            <Field name='username' type='text' placeholder='Type username' />
-            {touched.username && errors.username && (
-              <p>{errors.username}</p>
-            )}
-            <br />
-            <label>Password</label>
-            <Field name='password' type='password' placeholder='Type password' />
-            {touched.password && errors.password && (
-              <p>{errors.password}</p>
-            )}
-            <br />
-            <Link to='/search'><button type='submit'>Let's Go!</button></Link>
+          <label>Email</label>
+          <input
+            name="email"
+            type="text"
+            value={user.email}
+            onChange={handleChange}
+            placeholder="email..."
+          />
+          <br />
+          <label>Password</label>
+          <input
+            name="password"
+            type="password"
+            value={user.password}
+            onChange={handleChange}
+            placeholder="password..."
+          />
+          <br />
+          {/* <Link to="/search"> */}
+          <button type="submit">Let's Go!</button>
+          {/* </Link> */}
         </FormWrapper>
-      </Form>
+      </form>
     </SignInDiv>
-  )
-}
-
-const FormikSignIn = withFormik({
-  mapPropsToValues({ email, username, password }) {
-    return {
-      email: email || '',
-      username: username || '',
-      password: password || '',
-    }
-  },
-
-  validationSchema: Yup.object().shape({
-    email: Yup.string().email().required('Not a valid email address'),
-    username: Yup.string().required('Please choose a username'),
-    password: Yup.string().required('Please enter a password'),
-  }),
-
-  handleSubmit(values, { setStatus }) {
-    axios
-      .post('https://reqres.in/api/users', values)
-      .then(res => {
-        console.log(res);
-      })
-      .catch(err => console.log(err));
-  }
-})(SignIn);
-
-export default FormikSignIn;
+  );
+};
+export default connect(
+  null,
+  { login }
+)(SignIn);
