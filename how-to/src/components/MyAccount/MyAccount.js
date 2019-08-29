23998b50a0a990ebf8bd656fb3f790b466626db1
Elvis Knapman <elvis@elvisknapman.com>
2019-08-29 17:00:26 -0400

    :recycle: changed link path

--- how-to/src/components/MyAccount/MyAccount.js
+++ how-to/src/components/MyAccount/MyAccount.js
@@ -52,7 +52,7 @@ const MyAccount = ({
     <div>
       <h1>My Account</h1>
       <div>
-        <Link to="/editprofile">Edit Account Information</Link>
+        <Link to="/myaccount/editprofile">Edit Account Information</Link>
       </div>
       <div>
         <Link to={`${match.path}/mytutorials`}>My Tutorials</Link>
